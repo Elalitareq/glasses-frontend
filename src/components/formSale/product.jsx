ca49b54d1225ece3311fd5d62b1865745874dad7
Elalitareq <117711772+Elalitareq@users.noreply.github.com>
2023-06-20 12:21:23 +0300

    gg

--- src/components/formSale/product.jsx
+++ src/components/formSale/product.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FcAddRow } from 'react-icons/fc'
 import { CiCircleRemove } from 'react-icons/ci'
 
 function MyComboboxProduct({ onSelectedProducts }) {
   const [inputs, setInputs] = useState([{ id: 0, value: "" }]);
   const [nextId, setNextId] = useState(1);
+  const inputRefs = useRef([]);
 
   const handleInputChange = (index, event) => {
     const newInputs = [...inputs];
@@ -20,27 +21,16 @@ function MyComboboxProduct({ onSelectedProducts }) {
 
   const handleAddInput = (event) => {
     event.preventDefault(); // Prevent form submission
-  
+
     setInputs([...inputs, { id: nextId, value: "" }]);
     setNextId(nextId + 1);
   };
-  
 
-  const handleRemoveInput = (index) => {
-    if (index === 0) {
-      return; 
+  useEffect(() => {
+    if (inputRefs.current.length > 0) {
+      inputRefs.current[inputRefs.current.length - 1].focus();
     }
-    const newInputs = [...inputs];
-    newInputs.splice(index, 1);
-    setInputs(newInputs);
-
-    // Call onSelectedProducts with updated input values
-    if (typeof onSelectedProducts === "function") {
-      const selectedProducts = newInputs.map((input) => input.value);
-      onSelectedProducts(selectedProducts);
-    }
-  };
-
+  }, [inputs]);
 
   return (
     <div className="container py-2 text-black">
@@ -52,12 +42,11 @@ function MyComboboxProduct({ onSelectedProducts }) {
             value={input.value}
             onChange={(event) => handleInputChange(index, event)}
             className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+            ref={(ref) => (inputRefs.current[index] = ref)} // Assign the input reference to the corresponding index
           />
-          {index !== 0 && (
-            <button onClick={() => handleRemoveInput(index)}>
-              <CiCircleRemove className="w-10 h-10 text-white" />
-            </button>
-          )}
+          {/* <button onClick={() => handleRemoveInput(index)}>
+            <CiCircleRemove />
+          </button> */}
         </div>
       ))}
       <button onClick={handleAddInput} className="flex justify-center items-center text-white">
